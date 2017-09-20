d8bc7a188f8bdb320976d94b56ee69f896db93d0
rachael kiteme <rachaelrirrie@gmail.com>
2017-09-20 11:45:52 +0300

    methods

---
+++ method.rb
@@ -0,0 +1,10 @@
+class Dog
+def say(a, b, c)
+puts a
+puts b
+puts c
+end
+end
+my_dog = Dog.new
+my_dog.say("my dog", "is", "2oys")
+
---
+++ string.rb
@@ -0,0 +1,9 @@
+puts "am 20 years old".length
+puts "am 20 years old".class
+puts 50 .class
+puts "am 20 years old".upcase
+puts "am 20 years old".capitalize
+puts "am 20 years old".sum
+puts "am 20 years old".reverse
+puts "am 20 years old".upcase.reverse
+puts "am 20 years old".upcase.reverse.next
