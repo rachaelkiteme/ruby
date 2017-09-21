5b9f853ba0d8e7c13577d068b92d290d6c9de7b2
rachael kiteme <rachaelrirrie@gmail.com>
2017-09-21 11:35:14 +0300

    add substitution

---
+++ compare.rb
@@ -0,0 +1,11 @@
+age = 30
+puts "you are adult"if age>18
+age=15
+puts "you are a teenager" if age>10 && age<18
+age= 16
+puts "you are not a teenager" unless age>10 && age<18
+age = 30
+puts "you are 30yrs" if age == 30
+gender ="female"
+age = 50
+puts "she is a young lady" if gender =="female" && if (age > 18 || age < 25)
---
+++ sub.rb
@@ -0,0 +1,11 @@
+
+puts "foobar".sub('bar','foo')
+
+puts "this is a text".gsub('i', '')
+x="Rachael is my name"
+puts x.sub(/^../, "mercy")
+x="Rachael is my name"
+puts x.sub(/^..../, "mercy")
+x="Rachael is my name"
+puts x.sub(/..$/, "mercy")
+
